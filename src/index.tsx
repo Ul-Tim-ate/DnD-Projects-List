f5b76ad27d5e7253dee731f457872f98e83a47fc
Timofey Trakhanov <timohator1@mail.ru>
2022-12-02 12:05:48 +0300

    create firebase app and init in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.sass";
 import App from "./components/app/App";
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+  apiKey: "AIzaSyBAIa8OG0eqbPGBNpEV0To4J538G2Ex7Go",
+  authDomain: "task-manager-d9e1d.firebaseapp.com",
+  projectId: "task-manager-d9e1d",
+  storageBucket: "task-manager-d9e1d.appspot.com",
+  messagingSenderId: "427055786718",
+  appId: "1:427055786718:web:982f3b0fd08fabde9cb04f",
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
