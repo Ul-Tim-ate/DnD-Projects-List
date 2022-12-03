7dd18dd80385ae3037141c5ddf1ab4e890f13cc5
Timofey Trakhanov <timohator1@mail.ru>
2022-12-03 19:07:45 +0300

    add spinner

---
+++ src/components/UI/my-spinner/my-spinner.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MySpinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-44agqu7h2sf">
+      <div className="ldio-lhi5vf4o3t">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MySpinner;
