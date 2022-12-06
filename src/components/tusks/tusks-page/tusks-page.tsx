05cda29b61810b3a1eb6e2bb9fd4339c015ac919
Timofey Trakhanov <timohator1@mail.ru>
2022-12-06 16:51:49 +0300

    create responsive top of tusk-page

--- src/components/tusks/tusks-page/tusks-page.tsx
+++ src/components/tusks/tusks-page/tusks-page.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './tusks-page'
+import React, { useState } from "react";
+import Header from "../../header/header";
+import MyButton from "../../UI/my-button/my-button";
+import MyInput from "../../UI/my-input/my-input";
+import MyModal from "../../UI/my-modal/my-modal";
+import "./tusks-page.sass";
 
 const TusksPage = () => {
+  const [modalActive, setModalActive] = useState(false);
   return (
-    <div>TusksPage</div>
-  )
-}
+    <div className="tusks-page">
+      <Header />
+      <div className="container tusks-page__content">
+        <div className="tusks-page__content-top">
+          <h2 className="tusks-page__header">Ваши задачи</h2>
+          <MyButton value="Добавить задачу" activeModal={setModalActive} />
+        </div>
+        <div className="tusks-page__search">
+          <MyInput id="search-tusk" />
+        </div>
+      </div>
+      <MyModal active={modalActive} setActive={setModalActive}>
+        фывфыв
+      </MyModal>
+    </div>
+  );
+};
 
-export default TusksPage
+export default TusksPage;
